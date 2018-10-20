193e65eb8bd629a60442a31c87bf4f9dd965d1cb
Minhaz Rahman <minhaz.rtk@gmail.com>
2018-10-20 13:42:31 +0600

    Add Contact activity added...

---
+++ app/src/main/java/com/batch2/minhaz/batchcontact/AddContacts.java
@@ -0,0 +1,13 @@
+package com.batch2.minhaz.batchcontact;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddContacts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_contacts);
+    }
+}
