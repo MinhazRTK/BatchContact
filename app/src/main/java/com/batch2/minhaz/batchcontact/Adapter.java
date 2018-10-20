2872313088ce54c469e3485b8fc113558af15480
Minhaz Rahman <minhaz.rtk@gmail.com>
2018-10-20 15:29:40 +0600

    menu added...

--- app/src/main/java/com/batch2/minhaz/batchcontact/Adapter.java
+++ app/src/main/java/com/batch2/minhaz/batchcontact/Adapter.java
@@ -1,8 +1,17 @@
 package com.batch2.minhaz.batchcontact;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.database.sqlite.SQLiteDatabase;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -13,12 +22,40 @@ public class Adapter extends ArrayAdapter<Contacts> {
     List<Contacts> contactsList;
     SQLiteDatabase mDatabase;
 
-    public Adapter(Context context, int resource, Context mCtx, int listLayoutRes, List<Contacts> contactsList, SQLiteDatabase mDatabase) {
+    public Adapter(Context context, int resource, List<Contacts> contactsList, SQLiteDatabase mDatabase) {
         super(context, resource);
         this.mCtx = mCtx;
         this.listLayoutRes = listLayoutRes;
         this.contactsList = contactsList;
         this.mDatabase = mDatabase;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        LayoutInflater inflater = LayoutInflater.from(mCtx);
+        View view = inflater.inflate(listLayoutRes, null);
+
+        final Contacts employee = contactsList.get(position);
+
+
+        TextView textViewName = view.findViewById(R.id.name);
+        TextView textViewPhone = view.findViewById(R.id.phone);
+        TextView textViewEmail = view.findViewById(R.id.email);
+        TextView textViewAddress = view.findViewById(R.id.address);
+
+
+        textViewName.setText(employee.getName());
+        textViewPhone.setText(employee.getPhone());
+        textViewEmail.setText(String.valueOf(employee.getEmail()));
+        textViewAddress.setText(employee.getAddress());
+
+
+        Button buttonCall = view.findViewById(R.id.call);
+        Button buttonMessage = view.findViewById(R.id.message);
+        Button buttonWeb = view.findViewById(R.id.website);
+
+        return view;
     }
 
 }
--- app/src/main/java/com/batch2/minhaz/batchcontact/MainActivity.java
+++ app/src/main/java/com/batch2/minhaz/batchcontact/MainActivity.java
@@ -1,13 +1,63 @@
 package com.batch2.minhaz.batchcontact;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewConfiguration;
+import android.widget.ListView;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String DATABASE_NAME = "database.sqlite";
+
+    List<Contacts> contactsList;
+    SQLiteDatabase mDatabase;
+    ListView contactListView;
+    Adapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        contactListView = findViewById(R.id.contactListView);
+        contactsList = new ArrayList<>();
+
+        mDatabase = openOrCreateDatabase(MainActivity.DATABASE_NAME,MODE_PRIVATE,null);
+        showContactsFromDatabase();
+
+    }
+
+    private void showContactsFromDatabase() {
+
+        Cursor cursorContacts = mDatabase.rawQuery("SELECT * FROM contacts",null);
+
+        if(cursorContacts.moveToFirst()){
+            do {
+             contactsList.add(new Contacts(
+                     cursorContacts.getInt(0),
+                     cursorContacts.getString(1),
+                     cursorContacts.getString(2),
+                     cursorContacts.getString(3),
+                     cursorContacts.getString(4),
+                     cursorContacts.getString(5),
+                     cursorContacts.getString(6)
+             ));
+            }while (cursorContacts.moveToNext());
+        }
+
+        cursorContacts.close();
+
+        adapter = new Adapter(this,R.layout.view_contact,contactsList,mDatabase);
+
+        contactListView.setAdapter(adapter);
+
     }
 }
+
